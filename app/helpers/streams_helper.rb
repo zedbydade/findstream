b866f139e70cdfb79eed5af8f167ce4f445bab35
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-09-08 19:58:23 -0300

    Add some helper methods

--- app/helpers/streams_helper.rb
+++ app/helpers/streams_helper.rb
@@ -1,2 +1,27 @@
 module StreamsHelper
+  def get_language(value)
+    case value 
+    in 'en'
+    'English'
+    in 'de'
+    'German'
+    in 'it'
+    'Italian'
+    in 'ja'
+    'Japanese'
+    in 'ko'
+    'Korean'
+    in 'ru'
+    'Russian'
+    in 'es'
+    'Spanish'
+    end
+  end
+
+  def get_time(time)
+    diff = Time.diff(time.to_datetime, Time.now)
+    return "#{diff[:minute]} minutes" unless diff[:hour] > 1 
+    hour_string = diff[:hour] == 1 ? "hour" : "hours"
+    "#{diff[:hour]} #{hour_string} and #{diff[:minute]} minutes"
+  end
 end
