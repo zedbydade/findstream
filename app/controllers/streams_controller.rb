fdac0f110ce7b4b630be8109d5c9016ecdbb93b8
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-09-08 19:58:11 -0300

    Add select at streams controller

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,6 +1,10 @@
 class StreamsController < ApplicationController
   def index
     @streams = GetStreams.call(params)
+    @streams = @streams["data"].select {|stream|
+      String::Similarity.cosine(params[:search_string], stream["title"]) > 0.3
+    } unless params[:search_string].blank?
+    
   end
 
   def home; end
