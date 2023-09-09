f1e1ac2d2f6a520c5e6dbcfaeaa80fddac79dae1
Francisco Paradella <franciscoleite.dev@protonmail.com>
2023-09-09 19:15:36 -0300

    Add early return to stream_controller

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -1,10 +1,10 @@
 class StreamsController < ApplicationController
   def index
     @streams = GetStreams.call(params)
-    @streams = @streams["data"].select {|stream|
-      String::Similarity.cosine(params[:search_string], stream["title"]) > 0.3
+    return @streams = @streams["data"].select {|stream|
+      String::Similarity.cosine(params[:search_string], stream["title"]) > 0.5
     } unless params[:search_string].blank?
-    
+    @streams = @streams["data"] 
   end
 
   def home; end
